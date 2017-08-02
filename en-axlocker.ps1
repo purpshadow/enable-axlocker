171df865898784b12ca176d7687f20a815d0d84f
purpshadow <dannyel.ciobanica@axiomlaw.com>
2017-08-02 14:26:12 -0500

    Update en-axlocker.ps1

--- en-axlocker.ps1
+++ en-axlocker.ps1
@@ -14,15 +14,6 @@ $volume = Get-WmiObject win32_EncryptableVolume `
 # Get the target system's manufacturer. 
 $manufacturer = (get-wmiobject win32_computersystem).Manufacturer
 
-# If the manufacturer is HP, and the volume is not encrypted, prepare it.
-if ( $manufacturer -eq "Hewlett-Packard" -and ( $volume.encryptionmethod -eq 0 -or !$volume) ) {
-
-    $tpm = get-TPM
-
-    # Is the TPM not enabled? Enable it.
-    if ( $tpm.TpmReady -eq $false ) {
-        .\BiosConfigUtility64.exe /SetConfig:Enable-TPM.cfg /cspwd:biospassword /cspwd:""
-    }
 
     # Is there not an encryptable volume? Make C: encryptable with bdehdcfg.
     if ( -not $volume ) {
@@ -33,9 +24,4 @@ if ( $manufacturer -eq "Hewlett-Packard" -and ( $volume.encryptionmethod -eq 0 -
     if ($tpm.TpmReady -eq $true -and $volume ) {
         manage-bde -on c: -s -rp
     }   
-}
-
-# If this is a Lenovo machine that is not encrypted, encrypt it.
-if ( $manufacturer -eq "LENOVO" -and $volume.encryptionmethod -eq 0 ) {
-    manage-bde -on c: -s -rp
 }
